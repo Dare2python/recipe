a6fcb89c3e44ed0099ba731f02bcea73ae97f351
Dare2python <dare2python@gmail.com>
2018-07-31 19:18:41 +0300

    Subscribe in Recipe Detail

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -13,14 +13,24 @@ export class RecipeDetailComponent implements OnInit {
 
   // @Input() recipe: Recipe;
   recipe: Recipe;
+  id: number;
 
-  constructor(private recipeService: RecipeService,
+  constructor(
+    private recipeService: RecipeService,
     private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    const id: number = this.route.snapshot.params['id'];
-    this.recipe = this.recipeService.getRecipe(id);
+    this.id = this.route.snapshot.params['id'];
+    this.recipe = this.recipeService.getRecipe(this.id);
+
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = params['id'];
+          this.recipe = this.recipeService.getRecipe(this.id);
+        }
+      );
   }
 
 }
